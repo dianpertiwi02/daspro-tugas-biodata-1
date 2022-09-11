18505354b82004595fc82d99adb2e01ab273ef1f
dianpertiwi02 <113008951+dianpertiwi02@users.noreply.github.com>
2022-09-11 09:48:14 +0700

    Add files via upload

---
+++ tugasbiodata1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "dian pertiwi!" << endl;
+    cout << "A11.2022.14602!" << endl;
+    cout << "jl.gunung jati utara raya rt 04 rw 02 wonosari ngaliyan semaarang!" << endl;
+    cout << "kendal,15-02-2004!" << endl;
+    cout << "berenang,mendengarkan musik,berolahraga!" << endl;
+    return 0;
+}
