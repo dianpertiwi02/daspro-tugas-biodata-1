1bff89a6156f76b93f887627856dadc90f5debcc
dianpertiwi02 <113008951+dianpertiwi02@users.noreply.github.com>
2022-09-26 21:51:26 +0700

    Add files via upload

---
+++ tugas_luas_permukaan_balok/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+    Nama : Dian Pertiwi
+    NIM  : A11.2022.14602
+
+    luas_sisi_depan == luas_sisi_belakang
+    luas_sisi_kanan == luas_sisi_kiri
+    luas_sisi == luas_atap
+*/
+int main()
+{
+    float panjang, lebar, tinggi,
+luas_alas, luas_sisi_depan,
+luas_sisi_kanan, luas_permukaan_balok;
+
+    cout << "Masukan Panjang : ";
+    cin >> panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> lebar;
+    cout << "Masukkan tinggi : ";
+    cin >> tinggi;
+    luas_alas = panjang * lebar;
+    luas_sisi_depan = panjang * tinggi;
+    luas_sisi_kanan = lebar * tinggi;
+    luas_permukaan_balok = 2 * (luas_alas + luas_sisi_depan + luas_sisi_kanan);
+    cout << "Luas Permukaan Baloknya adalah : " << luas_permukaan_balok << endl;
+    return 0;
+}
