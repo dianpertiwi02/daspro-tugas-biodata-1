95135396f309455dcd637737a7a11bc62acac3a4
dianpertiwi02 <113008951+dianpertiwi02@users.noreply.github.com>
2022-11-23 21:10:17 +0700

    Add files via upload

---
+++ pertemuan 19/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    //loping 6-2
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+           cout << i << endl;
+        }
+    }
+
+int i,j;
+    for(i = 1,j=100; i <=99,j>=2; i++, j--) {
+            cout << i<<"" <<j << endl;
+        }*/
+
+    for(int i=1; i<=50; i++) {
+        int x= i*2;
+        cout << i << " --> " << x << endl;
+    }
+    return 0;
+}
