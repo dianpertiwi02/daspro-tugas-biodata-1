f32ef9fd928b788a97f4ef8eef72488f2a87b24a
dianpertiwi02 <113008951+dianpertiwi02@users.noreply.github.com>
2022-11-27 20:32:11 +0700

    Add files via upload

---
+++ pertemuan3/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    //printf("teks\n");
+
+
+    int u =20;
+    float t =155;
+    char ub ='m';
+    string ubs ="xl";
+
+    cout <<"umur saya: " << u;
+    cout << endl;
+    cout <<"tiinggi saya: " << t;
+    cout <<"\nukuran baju: " << ubs;
+
+    int a;
+    cout <<"\nMasukan angka: ";
+    cin >> a;
+    cout <<"Angka anda; "<< a + u;
+
+    return 0;
+}
