8a2b383cbe15cc2f6c8afd75c4591e01386304f7
dianpertiwi02 <113008951+dianpertiwi02@users.noreply.github.com>
2022-12-16 12:20:04 +0700

    Add files via upload

---
+++ pertemuan 22/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   /*
+   konversi detik
+    int x,secound,minutes,hours;
+    cout <<"\n\nInput: ";
+    cin >> x;
+
+    hours = x /3600;
+    x-= 3600 * hours;
+
+    minutes = x / 60;
+    x-= 60* minutes;
+
+    swap
+    int a = 5;
+    int b = 6;
+    int x;
+
+    x=a;
+    a=b;
+    b=x;
+
+    cout << a << " , " << b;
+
+    max
+    int a,b,c,maxi;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    if(a > b && a . c){
+        maxi = a;
+    }
+    if (b > a && b > c){
+        maxi = b;
+    }
+    if (c > a && c > b){
+        maxi = c;
+    }
+
+    cout << " nilai maxi: " << maxi;
+    */
+
+    return 0;
+}
+
